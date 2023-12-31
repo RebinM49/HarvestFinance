79890e5391ed51c43748e21d8d4642cdcb8806ef
Rebin <rebinm499@gmail.com>
2023-12-31 12:40:35 +0330

    Abstract Entity Type Created

---
+++ HarvestFinance.Domain/Common/Entity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestFinance.Domain.Common
+{
+    public abstract class Entity
+    {
+        public  Guid Id { get; set; }
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == Guid.Empty  || other.Id == Guid.Empty)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
