8402feae0ffd6e90af5db7fe6d0894b7f3956e01
Rebin <rebinm499@gmail.com>
2023-12-31 13:19:41 +0330

    property validation for AreaBasedProjects

--- HarvestFinance.Domain/Entities/AreaBasedProject.cs
+++ HarvestFinance.Domain/Entities/AreaBasedProject.cs
@@ -8,15 +8,27 @@ namespace HarvestFinance.Domain.Entities;
 
 public class AreaBasedProject : Project
 {
-    public int AreaUnitPrice { get; set; }
+    private int _areaUnitPrice;
+    public int AreaUnitPrice
+    {
+        get => _areaUnitPrice;
+        set
+        {
+            if (_areaUnitPrice < 1000_000)
+                throw new ArgumentOutOfRangeException(nameof(AreaUnitPrice)
+                    , message: "The value for area unit price is not valid.");
+            _areaUnitPrice = value;
+        }
+    }
     public override long Cost
     {
         get => _cost;
         protected set
         {
-            if (value > 10000)
-                _cost = value;
-            throw new ArgumentOutOfRangeException();
+            if (value < 10000)
+                throw new ArgumentOutOfRangeException(nameof(Cost) 
+                    ,message:"calculated value for cost is invalid due to wrong input");
+            _cost = value;
         }
     }
     public AreaBasedProject()
