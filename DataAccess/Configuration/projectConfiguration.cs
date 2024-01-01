bfc629cf96d717092ad416f6dbe14ee65cb864d3
Rebin <rebinm499@gmail.com>
2024-01-01 13:23:36 +0330

    projectConfiguration class

---
+++ DataAccess/Configuration/projectConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using HarvestFinance.Domain.Constants;
+using HarvestFinance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Configuration
+{
+    public class projectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.HasOne(p => p.Farmer)
+                .WithMany(f => f.Projects)
+                .HasForeignKey(p => p.FarmerId);
+            builder.HasDiscriminator(h => h.ContractType)
+                .HasValue<AreaBasedProject>(ContractType.AreaBased)
+                .HasValue<SharedBasedProject>(ContractType.SharedBased);
+        }
+    }
+}
