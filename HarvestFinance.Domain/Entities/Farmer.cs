44d5b5b8e72627441fe4f022ee3bd98dd30041c4
Rebin <rebinm499@gmail.com>
2024-01-01 16:13:16 +0330

    farmer entity updated

--- HarvestFinance.Domain/Entities/Farmer.cs
+++ HarvestFinance.Domain/Entities/Farmer.cs
@@ -17,10 +17,9 @@ namespace HarvestFinance.Domain.Entities
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public Guid ProjectId { get; set; }
 
         // Navigation Properties
-        public List<Project> Projects { get; set; }
+        public List<Project>? Projects { get; set; }
 
 
     }
