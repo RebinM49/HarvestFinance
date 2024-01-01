97018c640bf054960b6ef0f912cce028331a65a2
Rebin <rebinm499@gmail.com>
2024-01-01 09:18:35 +0330

    IRepository and IFarmerRepository , IProjectRepository added

---
+++ HarvestFinance.Domain/Repositories/IFarmerRepository.cs
@@ -0,0 +1,13 @@
+﻿using HarvestFinance.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestFinance.Domain.Repositories
+{
+    public interface IFarmerRepository :IRepository<Farmer>
+    {
+    }
+}
---
+++ HarvestFinance.Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using HarvestFinance.Domain.Common;
+using System.Linq.Expressions;
+
+
+namespace HarvestFinance.Domain.Repositories;
+
+ public interface IRepository<TEntity> where TEntity : Entity
+    {
+        TEntity Get(Guid Id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+    }
