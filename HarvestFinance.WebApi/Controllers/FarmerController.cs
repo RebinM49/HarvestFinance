2758dbccc95c717e595345d231aaa886cb8ebc31
Rebin <rebinm499@gmail.com>
2024-01-01 17:49:49 +0330

    farmer controller is working

--- HarvestFinance.WebApi/Controllers/FarmerController.cs
+++ HarvestFinance.WebApi/Controllers/FarmerController.cs
@@ -1,5 +1,6 @@
 ﻿using HarvestFinance.Domain;
 using HarvestFinance.Domain.Entities;
+using HarvestFinance.WebApi.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,16 +17,60 @@ public class FarmerController : ControllerBase
         _uow = unitofwork;
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Farmer>>> GetFarmers()
+    public async Task<ActionResult<IEnumerable<GetFarmerDto>>> GetFarmers()
     {
-        throw new NotImplementedException();
+        var farmers = await _uow.Farmers.GetAllAsync();
+        var farmerdto = farmers.Select(f
+                => new GetFarmerDto(f.Id, f.FirstName, f.LastName, f.PhoneNumber, f.Address)
+                );
+        return Ok(farmers);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetFarmerDto>> GetFarmer(Guid id)
+    {
+        var farmer = await _uow.Farmers.GetAsync(id);
+        if (farmer is null)
+            return NotFound();
+        var farmerdto = new GetFarmerDto(
+            farmer.Id, farmer.FirstName, farmer.LastName, farmer.PhoneNumber, farmer.Address);
+        return Ok(farmerdto);
     }
 
     [HttpPost]
-    public async Task<ActionResult<Farmer>> PostFarmer(Farmer farmer)
+    public async Task<ActionResult<Farmer>> PostFarmer(CreateFarmerDTO farmerDto)
     {
+        Farmer farmer = new()
+        {
+            FirstName = farmerDto.FirstName,
+            LastName = farmerDto.LastName,
+            PhoneNumber = farmerDto.PhoneNumber,
+            Address = farmerDto.Address
+        };
+
         await _uow.Farmers.AddAsync(farmer);
         await _uow.CompleteAsync();
-        return Ok(farmer);
+        return CreatedAtAction(nameof(GetFarmer), new { Id = farmer.Id }, farmerDto);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> PutFarmer(Guid id ,UpdateCustomerDto farmerdto)
+    {
+        var farmer = await _uow.Farmers.GetAsync(id);
+        if (farmer is null)
+            return NotFound();
+        farmer.FirstName = farmerdto.firstName;
+        farmer.LastName = farmerdto.lastName;
+        farmer.PhoneNumber = farmerdto.phoneNumber;
+        _uow.Farmers.Update(farmer);
+        try
+        {
+            await _uow.CompleteAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            throw;
+        }
+        return NoContent();
     }
 }
