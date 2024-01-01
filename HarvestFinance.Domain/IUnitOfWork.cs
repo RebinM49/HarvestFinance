3aa83392e728805b63b5315fbe1184ad0efe9051
Rebin <rebinm499@gmail.com>
2024-01-01 11:33:21 +0330

    Iunitofwork added

---
+++ HarvestFinance.Domain/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using HarvestFinance.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestFinance.Domain
+{
+    public interface IUnitOfWork
+    {
+        public IFarmerRepository FarmerRepository { get; }
+        public IProjectRepository ProjectRepository { get; }
+        public  Task<int> CompleteAsync();
+    }
+}
