295a04adece30da0af41a854feb56f8d1d08b622
Rebin <rebinm499@gmail.com>
2024-01-01 12:13:34 +0330

    farmer controller added

---
+++ HarvestFinance.WebApi/Controllers/FarmerController.cs
@@ -0,0 +1,31 @@
+﻿using HarvestFinance.Domain;
+using HarvestFinance.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HarvestFinance.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FarmerController : ControllerBase
+{
+    private readonly IUnitOfWork _uow;
+    public FarmerController(IUnitOfWork unitofwork)
+    {
+        _uow = unitofwork;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Farmer>>> GetFarmers()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Farmer>> PostFarmer(Farmer farmer)
+    {
+        await _uow.Farmers.AddAsync(farmer);
+        await _uow.CompleteAsync();
+        return Ok(farmer);
+    }
+}
