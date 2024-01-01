5749b5da9be7f293f18fb10c85fb4c91a9b6c632
Rebin <rebinm499@gmail.com>
2024-01-01 10:58:47 +0330

    Generic repository updated and farmer , project repository added as well

---
+++ DataAccess/Repositories/FarmerRepository.cs
@@ -0,0 +1,16 @@
+﻿using HarvestFinance.Domain.Entities;
+using HarvestFinance.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories;
+
+public class FarmerRepository : Repository<Farmer>, IFarmerRepository
+{
+    public HarvestFinanceDbcontext Dbcontext { get => _context; }
+    public FarmerRepository(HarvestFinanceDbcontext context)
+        : base(context) { }
+}
---
+++ DataAccess/Repositories/ProjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using HarvestFinance.Domain.Entities;
+using HarvestFinance.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories;
+
+public class ProjectRepository : Repository<Project>, IProjectRepository
+{
+    public HarvestFinanceDbcontext Dbcontext { get => _context; }
+    public ProjectRepository(HarvestFinanceDbcontext context)
+        : base(context) { }
+    
+}
