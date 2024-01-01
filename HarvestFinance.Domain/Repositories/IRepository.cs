0efd3139d9b6e1bfd074fc3459e99da31a524016
Rebin <rebinm499@gmail.com>
2024-01-01 09:57:08 +0330

    IRepository Updated

--- HarvestFinance.Domain/Repositories/IRepository.cs
+++ HarvestFinance.Domain/Repositories/IRepository.cs
@@ -4,13 +4,11 @@ using System.Linq.Expressions;
 
 namespace HarvestFinance.Domain.Repositories;
 
- public interface IRepository<TEntity> where TEntity : Common.Entity
-    {
-        Task<TEntity> Get(Guid Id);
-        Task<IEnumerable<TEntity>> GetAll();
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
-        Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
-
-        Task Add(TEntity entity);
-        Task Remove(TEntity entity);
-    }
+public interface IRepository<TEntity> where TEntity : Common.Entity
+{
+    Task<TEntity> Get(Guid Id);
+    Task<IEnumerable<TEntity>> GetAll();
+    Task<TEntity> Update(TEntity entity);
+    Task Add(TEntity entity);
+    Task Remove(TEntity entity);
+}
