6e765dffd0e7252c0c60df60a453c54df2d6d5f7
Rebin <rebinm499@gmail.com>
2024-01-01 09:09:45 +0330

    Infrastructure project - HarvestFinanceDbcontext

---
+++ DataAccess/HarvestFinanceDbcontext.cs
@@ -0,0 +1,26 @@
+﻿using HarvestFinance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+
+
+namespace DataAccess;
+
+public class HarvestFinanceDbcontext : DbContext
+{
+    public IConfiguration Configuration { get; }
+    public HarvestFinanceDbcontext(DbContextOptions options,IConfiguration config) 
+        : base(options)
+    {
+        Configuration = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("HarvestFinance_I")); 
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Farmer> Farmers { get; set; }
+    public DbSet<Project> Projects { get; set; }
+}
