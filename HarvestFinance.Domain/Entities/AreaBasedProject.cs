3b588d8e8c903d5ab6cfce21cfa339383cd730a6
Rebin <rebinm499@gmail.com>
2024-01-02 10:20:41 +0330

    ContractKind sets in constructor

--- HarvestFinance.Domain/Entities/AreaBasedProject.cs
+++ HarvestFinance.Domain/Entities/AreaBasedProject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HarvestFinance.Domain.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ public class AreaBasedProject : Project
     {
         Cost = CalculateCost();
         Date = DateTime.Now;
+        ContractKind = ContractType.AreaBased;
     }
     public int AreaUnitPrice
     {
