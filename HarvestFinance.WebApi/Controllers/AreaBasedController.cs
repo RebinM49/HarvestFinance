db3f73b5e37dd70b91a78ff933478bb225f76e4c
Rebin <rebinm499@gmail.com>
2024-01-02 11:26:38 +0330

    AreaBasedController created and post method works as expected

---
+++ HarvestFinance.WebApi/Controllers/AreaBasedController.cs
@@ -0,0 +1,41 @@
+﻿using HarvestFinance.Domain;
+using HarvestFinance.Domain.Entities;
+using HarvestFinance.WebApi.DTO.ProjectDto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace HarvestFinance.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AreaBasedController : ProjectController
+    {
+        public AreaBasedController(IUnitOfWork uow) : base(uow)
+        {
+        }
+        [HttpPost]
+        public async Task<ActionResult<GetProjectDto>> PostAreaBasedProject(CreateAreaBasedDto createAreaDto)
+        {
+            var AreaBased = MapDtoToModel(createAreaDto);
+            await _uow.Projects.AddAsync(AreaBased);
+            await _uow.CompleteAsync();
+            return CreatedAtAction(nameof(GetProject), new { Id = AreaBased.Id },AreaBased);
+        }
+
+        private AreaBasedProject MapDtoToModel(CreateAreaBasedDto dto)
+        {
+            var project = new AreaBasedProject(dto.FarmerId,
+                0,
+                dto.Area,
+                dto.ProductType,
+                dto.HarvestType,
+                dto.Address,
+                dto.CombineName,
+                dto.AreaUnitPrice
+                );
+            return project;
+
+        }
+    }
+}
