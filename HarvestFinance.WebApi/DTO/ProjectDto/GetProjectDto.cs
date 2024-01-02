05ea9ba2ece6c8357a20842d2733187d75f6f126
Rebin <rebinm499@gmail.com>
2024-01-02 09:51:20 +0330

    GetProjectDto type created

---
+++ HarvestFinance.WebApi/DTO/ProjectDto/GetProjectDto.cs
@@ -0,0 +1,17 @@
+﻿using HarvestFinance.Domain.Constants;
+
+namespace HarvestFinance.WebApi.DTO.ProjectDto;
+
+public record GetProjectDto(
+    Guid Id,
+    Guid FarmerId,
+    double Weight,
+    double Area,
+    DateTime Date,
+    ProductType Product,
+    HarvestType HarvestType,
+    long Cost,
+    ContractType Contract,
+    string Address
+    )
+{ }
