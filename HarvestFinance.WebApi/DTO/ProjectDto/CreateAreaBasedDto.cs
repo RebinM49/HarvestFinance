cb43884dee693a9abbb81697144763ddca554190
Rebin <rebinm499@gmail.com>
2024-01-02 11:25:53 +0330

    CreateAreaBaseDto Created

---
+++ HarvestFinance.WebApi/DTO/ProjectDto/CreateAreaBasedDto.cs
@@ -0,0 +1,17 @@
+﻿using HarvestFinance.Domain.Constants;
+
+namespace HarvestFinance.WebApi.DTO.ProjectDto
+{
+    public record CreateAreaBasedDto
+    (
+        Guid FarmerId,
+        double Area,
+        ProductType ProductType,
+        HarvestType HarvestType,
+        string Address,
+        string CombineName,
+        int AreaUnitPrice
+
+    )
+    { }
+}
