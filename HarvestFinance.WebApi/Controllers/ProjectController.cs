35b2a270631f7f49c56f9baaf381ad64ddc8ccdc
Rebin <rebinm499@gmail.com>
2024-01-02 12:01:28 +0330

    map method changed to return the string value

--- HarvestFinance.WebApi/Controllers/ProjectController.cs
+++ HarvestFinance.WebApi/Controllers/ProjectController.cs
@@ -5,60 +5,53 @@ using HarvestFinance.WebApi.DTO;
 using HarvestFinance.WebApi.DTO.ProjectDto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Extensions.Hosting;
-using System.Diagnostics.Contracts;
-using System.Net;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace HarvestFinance.WebApi.Controllers
+
+namespace HarvestFinance.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProjectController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProjectController : ControllerBase
+    protected readonly IUnitOfWork _uow;
+    public ProjectController(IUnitOfWork uow)
     {
-        protected readonly IUnitOfWork _uow;
-        public ProjectController(IUnitOfWork uow)
-        {
-            _uow = uow;
-        }
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<GetProjectDto>>> GetProjects()
-        {
-            var projects = await _uow.Projects.GetAllAsync();
-            var projectDto = projects.Select(p => MapToDto(p));
-            return Ok(projectDto);
-        }
+        _uow = uow;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<GetProjectDto>>> GetProjects()
+    {
+        var projects = await _uow.Projects.GetAllAsync();
+        var projectDto = projects.Select(p => MapToDto(p));
+        return Ok(projectDto);
+    }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<GetProjectDto>> GetProject(Guid id)
-        {
-            var project = await _uow.Projects.GetAsync(id);
-            if (project is null)
-                return NotFound();
-            var dto = MapToDto(project);
-            return Ok(dto);
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetProjectDto>> GetProject(Guid id)
+    {
+        var project = await _uow.Projects.GetAsync(id);
+        if (project is null)
+            return NotFound();
+        var dto = MapToDto(project);
+        return Ok(dto);
 
-        }
+    }
 
-        private GetProjectDto MapToDto(Project project)
-        {
-            var dto = new GetProjectDto(
-                project.Id,
-                project.FarmerId,
-                project.Weight,
-                project.Area,
-                project.Date,
-                project.ProductType,
-                project.HarvestType,
-                project.Cost,
-                project.ContractKind,
-                project.Address
-                );
+    private GetProjectDto MapToDto(Project project)
+    {
+        var dto = new GetProjectDto(
+            project.Id,
+            project.FarmerId,
+            project.Weight,
+            project.Area,
+            project.Date,
+            project.ProductType.ToString(),
+            project.HarvestType.ToString(),
+            project.Cost,
+            project.ContractKind.ToString(),
+            project.Address
+            );
 
-            return dto;
-        }
+        return dto;
     }
 }
