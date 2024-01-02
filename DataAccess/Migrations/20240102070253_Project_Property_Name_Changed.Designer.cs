bd2291837a60af4e105aae4fbba2cd80321c8386
Rebin <rebinm499@gmail.com>
2024-01-02 11:27:04 +0330

    Migration files update

--- DataAccess/Migrations/20240102070253_Project_Property_Name_Changed.Designer.cs
+++ DataAccess/Migrations/20240102070253_Project_Property_Name_Changed.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(HarvestFinanceDbcontext))]
-    partial class HarvestFinanceDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20240102070253_Project_Property_Name_Changed")]
+    partial class Project_Property_Name_Changed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
