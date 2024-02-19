ff8e8fc485a4557bae281dae164442cbdde0cf3b
Rebin <rebinm499@gmail.com>
2024-02-19 12:49:54 +0330

    Add : Farmer model classes for update added

--- HarvestFinance.WebApi_V2/Models/FarmerDto.cs
+++ HarvestFinance.WebApi_V2/Models/FarmerDto.cs
@@ -2,6 +2,7 @@
 {
     public class FarmerDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Address { get; set;} = string.Empty;
