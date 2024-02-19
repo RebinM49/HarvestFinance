a2e0e1a3e0936536eecdf4b36145cae6e0002b0f
Rebin <rebinm499@gmail.com>
2024-02-19 11:21:25 +0330

    Add : Farmer Model Dtos Created

---
+++ HarvestFinance.WebApi_V2/Models/FarmerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HarvestFinance.WebApi_V2.Models
+{
+    public class FarmerDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set;} = string.Empty;
+
+    }
+}
---
+++ HarvestFinance.WebApi_V2/Models/FarmerForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HarvestFinance.WebApi_V2.Models
+{
+    public class FarmerForCreationDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        
+    }
+}
