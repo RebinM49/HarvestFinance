e8d7fc21535b4ec7ff36ee45d88a8e36d6d387b3
Rebin <rebinm499@gmail.com>
2024-02-19 11:22:25 +0330

    Add : Farmer Controller with a Get action created

---
+++ HarvestFinance.WebApi_V2/Controllers/FarmerController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HarvestFinance.Domain.Repositories;
+using HarvestFinance.WebApi_V2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HarvestFinance.WebApi_V2.Controllers
+{
+    [Route("api/farmers")]
+    [ApiController]
+    public class FarmerController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IFarmerRepository _farmerRepo;
+
+        public FarmerController(IMapper mapper,IFarmerRepository farmerRepo)
+        {
+            _mapper = mapper;
+            _farmerRepo = farmerRepo;
+        }
+
+        [HttpGet(Name ="GetAllFarmers")]
+        public async Task<ActionResult<IEnumerable<FarmerDto>>> GetFarmers()
+        {
+            var farmers = await _farmerRepo.GetAllAsync();
+            
+            return Ok(_mapper.Map<IEnumerable<FarmerDto>>(farmers));
+        }
+
+
+    }
+}
