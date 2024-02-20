383041602cdae82c00a0e96c8773c4429d7e5c5d
Rebin <rebinm499@gmail.com>
2024-02-20 13:01:24 +0330

    Naming Fix

--- HarvestFinance.WebApi_V2/Models/FarmerForCreationDto.cs
+++ HarvestFinance.WebApi_V2/Models/FarmerForCreationDto.cs
@@ -7,6 +7,6 @@ namespace HarvestFinance.WebApi_V2.Models
         public string LastName { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-        
+                
     }
 }
