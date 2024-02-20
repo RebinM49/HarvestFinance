1d164d823f95e461001b1597bbb99ccf1ab7e476
Rebin <rebinm499@gmail.com>
2024-02-20 13:03:36 +0330

    Feature Add : Project Resource Controller added and Get action methods implemented

---
+++ HarvestFinance.WebApi_V2/Controllers/ProjectController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using HarvestFinance.Domain.Repositories;
+using HarvestFinance.WebApi_V2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HarvestFinance.WebApi_V2.Controllers;
+
+[Route( "api/farmers/{farmerId}/projects" )]
+[ApiController]
+public class ProjectController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IProjectRepository _projectRepo;
+
+    public ProjectController(IMapper mapper , IProjectRepository projectRepo)
+    {
+        _mapper = mapper;
+        _projectRepo = projectRepo;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjects([FromRoute] Guid farmerId)
+    {
+        if (!await _projectRepo.FarmerExists( farmerId ))
+        {
+            return NotFound( farmerId );
+        }
+        var projects = await _projectRepo.GetProjectsForFarmer( farmerId );
+        var projectsToReturn = _mapper.Map<IEnumerable<ProjectDto>>( projects );
+
+        return Ok( projectsToReturn );
+    }
+
+    [HttpGet( "{projectId}" )]
+    public async Task<ActionResult<ProjectDto>> GetProject(Guid farmerId , Guid projectId)
+    {
+        if (!await _projectRepo.FarmerExists( farmerId ))
+        {
+            return NotFound();
+        }
+        var project = await _projectRepo.GetAsync( projectId );
+        if (project is null)
+        {
+            return NotFound();
+        }
+        return Ok( _mapper.Map<ProjectDto>( project ) );
+    }
+
+}
