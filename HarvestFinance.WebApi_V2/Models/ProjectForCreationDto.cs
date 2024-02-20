3998c41a312318a41a05a9523668ddeda78f1e1d
Rebin <rebinm499@gmail.com>
2024-02-20 13:02:14 +0330

    Feature Add : new Dto classes added for project resourse endpoint

---
+++ HarvestFinance.WebApi_V2/Models/ProjectForCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using HarvestFinance.Domain.Constants;
+
+namespace HarvestFinance.WebApi_V2.Models
+{
+    public class ProjectForCreationDto
+    {
+
+        public double Weight { get; set; }
+        public double Area { get; set; }
+        public ProductType ProductType { get; set; }
+        public HarvestType HarvestType { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string CombineName { get; set; } = string.Empty;
+        public ContractType ContractKind { get; set; }
+    }
+}
