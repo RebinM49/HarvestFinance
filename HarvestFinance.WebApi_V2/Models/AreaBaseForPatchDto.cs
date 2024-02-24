9a2728664d5944aef22b4f2c1106fed55f225ced
Rebin <rebinm499@gmail.com>
2024-02-24 19:01:52 +0330

    Add : New Patch Dto class for AreaBase projects

---
+++ HarvestFinance.WebApi_V2/Models/AreaBaseForPatchDto.cs
@@ -0,0 +1,20 @@
+﻿using HarvestFinance.Domain.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace HarvestFinance.WebApi_V2.Models;
+
+public class AreaBaseForPatchDto
+{
+    [Required]
+    [Range( 0 , 4 )]
+    public ProductType ProductType { get; set; }
+    [Required]
+    [Range( 0 , 1 )]
+    public HarvestType HarvestType { get; set; }
+    [Required]
+    [Range( 1_500_000 , 50_000_000 )]
+    public int UnitPrice { get; set; }
+    [Required]
+    [Range( 0.2 , 100 )]
+    public double Area { get; set; }
+}
